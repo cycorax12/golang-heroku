8dbfa8254e61e384d308d37da312e30e8cd39f37
cycorax <oswalvirendra1@gmail.com>
2021-07-18 13:43:44 +0530

    Updated main.go to read POROT value

--- main.go
+++ main.go
@@ -1,13 +1,29 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+const PORT = "3000"
 
 func main() {
+	port := getEnv("PORT", PORT)
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("[golang] Hello from Heroku!")
 	})
 
-	app.Listen(":3000")
+	app.Listen(fmt.Sprintf(":%s", port))
+
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
 }
